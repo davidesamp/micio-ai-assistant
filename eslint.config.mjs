f3d6a20ce91611637096b5b9cbf441bca238213e
Davide Gaggero <davide.gaggero@gmail.com>
2025-04-16 23:20:19 +0200

    chore: added process in eslint globals

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -32,6 +32,7 @@ export default [
                 "localStorage": true,
                 "fetch": true,
                 "console": true,
+                "process": true,
             },
         },
         plugins: {
